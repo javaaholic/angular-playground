952654effa5cd5aa4872843501106d8b152fc93a
javaaholic <witnessme.jin@gmail.com>
2017-03-10 23:51:47 +0900

    complete 6.Routing

--- angular-tour-of-heroes/src/app/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 import { Hero } from './hero';
@@ -8,10 +8,11 @@ import 'rxjs/add/operator/switchMap';
 @Component({
   moduleId: module.id,
   selector: 'my-hero-detail',
-  templateUrl: './hero-detail.component.html'
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero: Hero;
+  hero: Hero;
 
   constructor(
     private heroService: HeroService,
