909d742166651f665650f36c83809aa5775d7e58
javaaholic <witnessme.jin@gmail.com>
2017-04-13 14:15:43 +0900

    http 모듈 추가

---
+++ angular-tour-of-heroes/src/app/in-memory-data.service.ts
@@ -0,0 +1,18 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    let heroes = [
+      { id: 11, name: '아즈모단' },
+      { id: 12, name: '자가라' },
+      { id: 13, name: '아르타니스' },
+      { id: 14, name: '타이커스' },
+      { id: 15, name: '디아블로' },
+      { id: 16, name: '아서스' },
+      { id: 17, name: '케리건' },
+      { id: 18, name: '노바' },
+      { id: 19, name: '레이너' },
+      { id: 20, name: '제라툴' }
+    ];
+    return {heroes};
+  }
+}
