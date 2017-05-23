fd09ace3f175fec75bc3650bd242fb5ea2e5e421
javaaholic <witnessme.jin@gmail.com>
2017-05-23 23:57:34 +0900

    메뉴, 툴바, 아이콘 추가

--- ng-material/src/app/app.module.ts
+++ ng-material/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MdButtonModule } from '@angular/material';
+import { MdButtonModule, MdToolbarModule, MdMenuModule, MdIconModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -18,6 +18,9 @@ import { AppComponent } from './app.component';
     HttpModule,
     BrowserAnimationsModule,
     MdButtonModule,
+    MdToolbarModule,
+    MdMenuModule,
+    MdIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
